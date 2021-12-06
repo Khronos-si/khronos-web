ec4402651927a4d1ebd9c84ce8358f2ae0b18a88
nikp00 <nik.princic@gmail.com>
2021-12-06 17:45:22 +0100

    Changed conf to work with reverse proxy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,31 +6,37 @@ module.exports = {
     loaderOptions: {
       sass: {
         sassOptions: {
-          includePaths: ['./node_modules', './src/assets']
-        }
-      }
-    }
+          includePaths: ['./node_modules', './src/assets'],
+        },
+      },
+    },
   },
   configureWebpack: {
     resolve: {
       alias: {
         '@themeConfig': path.resolve(__dirname, 'themeConfig.js'),
         '@core': path.resolve(__dirname, 'src/@core'),
-        '@validations': path.resolve(__dirname, 'src/@core/utils/validations/validations.js'),
-        '@axios': path.resolve(__dirname, 'src/libs/axios')
-      }
-    }
+        '@validations': path.resolve(
+          __dirname,
+          'src/@core/utils/validations/validations.js'
+        ),
+        '@axios': path.resolve(__dirname, 'src/libs/axios'),
+      },
+    },
   },
-  chainWebpack: config => {
+  devServer: {
+    disableHostCheck: true,
+  },
+  chainWebpack: (config) => {
     config.module
       .rule('vue')
       .use('vue-loader')
       .loader('vue-loader')
-      .tap(options => {
+      .tap((options) => {
         // eslint-disable-next-line no-param-reassign
         options.transformAssetUrls = {
-          'img': 'src',
-          'image': 'xlink:href',
+          img: 'src',
+          image: 'xlink:href',
           'b-avatar': 'src',
           'b-img': 'src',
           'b-img-lazy': ['src', 'blank-src'],
@@ -38,10 +44,10 @@ module.exports = {
           'b-card-img': 'src',
           'b-card-img-lazy': ['src', 'blank-src'],
           'b-carousel-slide': 'img-src',
-          'b-embed': 'src'
+          'b-embed': 'src',
         }
         return options
       })
   },
-  transpileDependencies: ['vue-echarts', 'resize-detector']
+  transpileDependencies: ['vue-echarts', 'resize-detector'],
 }
