cc085560c5a4c269d376d70efead166f47a34669
Tim Retelj <tim.retelj@it-team.si>
2021-12-14 23:14:00 +0100

    Added home screen and fixed white/dark theme on home

--- frontend/src/views/Components/ClockWithTask.vue
+++ frontend/src/views/Components/ClockWithTask.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>
-        <canvas id="clock" style="background: #2e3134;"> </canvas>
+    <div class="d-flex justify-content-center">
+        <canvas id="clock" :style=" isDark == true? 'background: #2e3134 !important;': 'background: white;'"> </canvas>
     </div>
 </template>
 
 <script>
+    import useAppConfig from '@core/app-config/useAppConfig'
+    import { computed } from '@vue/composition-api'
+    
     export default {
         data() {
             return {
@@ -15,10 +18,164 @@
 
             }
         },
+        setup() {
+            const { skin } = useAppConfig()
+
+            const isDark = computed(() => skin.value === 'dark')
+
+            return { skin, isDark }
+        },
         methods:{
             toRadians(degrees)             {
                 const pi = Math.PI
                 return (degrees - 90) * (pi / 180)
+            },
+            drawWatch() {
+                this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+                this.center = this.ctx.canvas.height / 2
+                this.r = (this.ctx.canvas.height / 2) * 0.7
+
+                // rdeca fe5c36
+                // MODRA 53afbe
+                // RUMENA feb449
+
+                //Sama kroznica ura
+                this.ctx.beginPath()
+                if (this.isDark) this.ctx.strokeStyle = 'white'
+                else this.ctx.strokeStyle = '#2e3134'
+
+                this.ctx.lineWidth = 3
+                this.ctx.arc(this.center, this.center, this.r, 0, 2 * Math.PI)
+                this.ctx.stroke()
+         
+                //Digitalni prikaz ure
+                // this.ctx.textAlign = 'center'
+                // this.ctx.font = '95px Arial'
+                // this.ctx.fillStyle = 'white'
+                // this.ctx.fillText('12:40', this.center, this.center + 33)
+
+                // this.ctx.strokeStyle = '#feb449'
+
+                // Urni Kazalec
+                this.ctx.save()
+                this.ctx.translate(this.center, this.center)
+                this.ctx.rotate(30 * Math.PI / 180)
+                this.ctx.beginPath()       
+                this.ctx.lineCap = 'round'
+                if (this.isDark) this.ctx.fillStyle = '#2e3134'
+                else this.ctx.fillStyle = 'white'
+
+                this.ctx.roundRect(0, -5, 90, 10, 10)
+                this.ctx.fill()
+                this.ctx.stroke()
+                this.ctx.restore()
+
+
+                const kot = 350
+            
+                // Minutni Kazalec
+                this.ctx.save()
+                this.ctx.translate(this.center, this.center)
+                this.ctx.rotate((-90 + kot) * Math.PI / 180)
+                this.ctx.beginPath()       
+                this.ctx.lineCap = 'round'
+                if (this.isDark) this.ctx.fillStyle = '#2e3134'
+                else this.ctx.fillStyle = 'white'
+                this.ctx.roundRect(0, -5, 120, 10, 10)
+                this.ctx.fill()
+                if (this.isDark) this.ctx.strokeStyle = 'white'
+                else this.ctx.strokeStyle = '#2e3134'
+                this.ctx.stroke()
+                this.ctx.restore()
+
+                // Krogec v sredini
+                this.ctx.beginPath()
+            
+
+                this.ctx.lineWidth = 10
+                this.ctx.arc(this.center, this.center, 5, 0, 2 * Math.PI)
+
+                if (this.isDark) this.ctx.strokeStyle = 'white'
+                else this.ctx.strokeStyle = '#2e3134'
+                this.ctx.stroke()
+            
+                if (this.isDark) this.ctx.fillStyle = '#2e3134'
+                else this.ctx.fillStyle = 'white'
+
+                this.ctx.fill()
+            
+            
+                this.ctx.lineWidth = 3
+
+                //Pikice
+                for (let i = 0; i < 360; i += 360 / 12) {
+
+                    let polna = 0
+
+                    if (i % 90 === 0) {
+                        polna = 4
+                    }
+
+                    this.ctx.beginPath()
+                    const miniCircleX = (Math.sin(this.toRadians(i)) * (this.r - 30)) + this.center
+                    const miniCircleY = (Math.cos(this.toRadians(i)) * (this.r - 30)) + this.center
+                    this.ctx.arc(miniCircleX, miniCircleY, 4 + polna, 0, 2 * Math.PI)
+
+                    if (this.isDark) this.ctx.fillStyle = 'white'
+                    else this.ctx.fillStyle = '#2E3134'
+
+                    this.ctx.fill()
+
+                    if (this.isDark) this.ctx.strokeStyle = 'white'
+                    else this.ctx.strokeStyle = '#2E3134'
+
+                    this.ctx.stroke()
+                
+                }
+
+
+                this.ctx.lineCap = 'round'
+                this.ctx.beginPath()
+                this.ctx.strokeStyle = 'rgba(5, 183, 237, 0.15)'
+                this.ctx.lineWidth = 6
+                this.ctx.arc(this.center, this.center, this.r + 25, this.toRadians(0), this.toRadians(90))
+
+
+                this.ctx.stroke()
+                this.ctx.beginPath() 
+                this.ctx.strokeStyle = '#ed8105'
+                this.ctx.lineWidth = 10
+                this.ctx.arc(this.center, this.center, this.r + 25, this.toRadians(105), this.toRadians(140))
+                this.ctx.stroke()
+
+                this.ctx.stroke()
+                this.ctx.beginPath()
+                this.ctx.strokeStyle = 'rgba(245, 108, 17, 0.15)'
+                this.ctx.lineWidth = 6
+                this.ctx.arc(this.center, this.center, this.r + 25, this.toRadians(170), this.toRadians(250))
+                this.ctx.stroke()
+
+                this.ctx.stroke()
+                this.ctx.beginPath()
+                this.ctx.strokeStyle = '#47ed05'
+                this.ctx.lineWidth = 10
+                this.ctx.arc(this.center, this.center, this.r + 25, this.toRadians(270), this.toRadians(340))
+                this.ctx.stroke()
+
+                this.ctx.stroke()
+                this.ctx.beginPath()
+                this.ctx.strokeStyle = '#edd605'
+                this.ctx.lineWidth = 10
+                this.ctx.arc(this.center, this.center, this.r + (50), this.toRadians(40), this.toRadians(170))
+                this.ctx.stroke()
+
+                this.ctx.stroke()
+                this.ctx.beginPath()
+                this.ctx.strokeStyle = '#2fa4a8'
+                this.ctx.lineWidth = 10
+                this.ctx.arc(this.center, this.center, this.r + (50), this.toRadians(200), this.toRadians(360))
+                this.ctx.stroke()
             }
         },
         mounted() {
@@ -35,138 +192,30 @@
                 this.closePath()
                 return this
             }
+
             this.canvas = document.getElementById('clock')
             this.ctx = this.canvas.getContext('2d')
-            console.log(this.ctx.canvas.width)
-            this.ctx.canvas.width  = 500
-            this.ctx.canvas.height  = 500
 
-            this.center = this.ctx.canvas.height / 2
-            this.r = (this.ctx.canvas.height / 2) * 0.7
+            let width = window.innerWidth * 0.8
 
-            // rdeca fe5c36
-            // MODRA 53afbe
-            // RUMENA feb449
+            if (width > 400) width = 500
 
-            //Sama kroznica ura
-            this.ctx.beginPath()
-            this.ctx.strokeStyle = '#fff'
-            this.ctx.lineWidth = 3
-            this.ctx.arc(this.center, this.center, this.r, 0, 2 * Math.PI)
-            this.ctx.stroke()
-         
-            //Digitalni prikaz ure
-            // this.ctx.textAlign = 'center'
-            // this.ctx.font = '95px Arial'
-            // this.ctx.fillStyle = '#fff'
-            // this.ctx.fillText('12:40', this.center, this.center + 33)
-
-            // this.ctx.strokeStyle = '#feb449'
-
-            // Urni Kazalec
-            this.ctx.save()
-            this.ctx.translate(this.center, this.center)
-            this.ctx.rotate(30 * Math.PI / 180)
-            this.ctx.beginPath()       
-            this.ctx.lineCap = 'round'
-            this.ctx.fillStyle = '#2e3134'
-            this.ctx.roundRect(0, -5, 90, 10, 10)
-            this.ctx.fill()
-            this.ctx.stroke()
-            this.ctx.restore()
+            this.ctx.canvas.width  = width
+            this.ctx.canvas.height = width
 
 
-            const kot = 350
-            
-            // Minutni Kazalec
-            this.ctx.save()
-            this.ctx.translate(this.center, this.center)
-            this.ctx.rotate((-90 + kot) * Math.PI / 180)
-            this.ctx.beginPath()       
-            this.ctx.lineCap = 'round'
-            this.ctx.fillStyle = '#2e3134'
-            this.ctx.roundRect(0, -5, 120, 10, 10)
-            this.ctx.fill()
-            this.ctx.stroke()
-            this.ctx.restore()
+            // this.ctx.canvas.width  = 500
+            // this.ctx.canvas.height  = 500
 
-
-            // Krogec v sredini
-            this.ctx.beginPath()
-            this.ctx.strokeStyle = '#fff'
-            this.ctx.lineWidth = 10
-            this.ctx.arc(this.center, this.center, 5, 0, 2 * Math.PI)
-            this.ctx.stroke()
-            this.ctx.fillStyle = '#2e3134'
-            this.ctx.fill()
-            this.ctx.fillStyle = '#fff'
-
-            this.ctx.lineWidth = 3
-            //Pikice
-            for (let i = 0; i < 360; i += 360 / 12) {
-
-                let polna = 0
-
-                if (i % 90 === 0) {
-                    polna = 4
-                }
-
-                this.ctx.beginPath()
-                const miniCircleX = (Math.sin(this.toRadians(i)) * (this.r - 30)) + this.center
-                const miniCircleY = (Math.cos(this.toRadians(i)) * (this.r - 30)) + this.center
-                this.ctx.arc(miniCircleX, miniCircleY, 4 + polna, 0, 2 * Math.PI)
-                this.ctx.stroke()
-                this.ctx.fillStyle = '#fff'
-                this.ctx.fill()
-                
+            this.drawWatch()
+        },
+        watch: {
+            isDark() {
+                this.drawWatch()
             }
-
-
-            this.ctx.lineCap = 'round'
-            this.ctx.beginPath()
-            this.ctx.strokeStyle = 'rgba(5, 183, 237, 0.15)'
-            this.ctx.lineWidth = 6
-            this.ctx.arc(this.center, this.center, this.r + 25, this.toRadians(0), this.toRadians(90))
-
-
-            this.ctx.stroke()
-            this.ctx.beginPath() 
-            this.ctx.strokeStyle = '#ed8105'
-            this.ctx.lineWidth = 10
-            this.ctx.arc(this.center, this.center, this.r + 25, this.toRadians(105), this.toRadians(140))
-            this.ctx.stroke()
-
-            this.ctx.stroke()
-            this.ctx.beginPath()
-            this.ctx.strokeStyle = 'rgba(245, 108, 17, 0.15)'
-            this.ctx.lineWidth = 6
-            this.ctx.arc(this.center, this.center, this.r + 25, this.toRadians(170), this.toRadians(250))
-            this.ctx.stroke()
-
-            this.ctx.stroke()
-            this.ctx.beginPath()
-            this.ctx.strokeStyle = '#47ed05'
-            this.ctx.lineWidth = 10
-            this.ctx.arc(this.center, this.center, this.r + 25, this.toRadians(270), this.toRadians(340))
-            this.ctx.stroke()
-
-            this.ctx.stroke()
-            this.ctx.beginPath()
-            this.ctx.strokeStyle = '#edd605'
-            this.ctx.lineWidth = 10
-            this.ctx.arc(this.center, this.center, this.r + (50), this.toRadians(40), this.toRadians(170))
-            this.ctx.stroke()
-
-            this.ctx.stroke()
-            this.ctx.beginPath()
-            this.ctx.strokeStyle = '#2fa4a8'
-            this.ctx.lineWidth = 10
-            this.ctx.arc(this.center, this.center, this.r + (50), this.toRadians(200), this.toRadians(360))
-            this.ctx.stroke()
-
-
         }
     }
+    
 </script>
 
 <style scoped>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,47 +1,247 @@
 <template>
     <div>
-        <b-card title="Kick start your project" class="w-100">
-            <b-card-text>All the best for your new project. Test</b-card-text>
-            <b-card-text
-            >Please make sure to read our
-                <b-link
-                    href="https://pixinvent.com/demo/vuexy-vuejs-admin-dashboard-template/documentation/"
-                    target="_blank">
-                    Template Documentation
-                </b-link>
-                to understand where to go from here and how to use our
-                template.</b-card-text>
-            <b-card-text class="text-center">
-                <fa class="text-danger" size="2x" icon="air-freshener" />
-                Usage of font-awesome-icons
-                <fa class="text-success" size="2x" icon="air-freshener" />
-            </b-card-text>
-        </b-card>
+        <div class="row">
+            <div class="col-lg-3 col-1">
+                <b-card class="w-100 h-100 mojCard">
+                    <div class="mb-2" style="font-size: 1.5rem;">Upcoming taski</div>
+                    <div class="text-left d-flex justify-content-between w-100" >
+                        <div>
+                            Grupa 1234
+                            <div style="font-size: 0.7rem;">Test test edit 12345</div>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-end ml-1">
+                            <span class="badge badge-pill" style="background: #53afbe !important; margin-bottom: 5px;">20.8.2020</span>
+                        </div>
+                    </div>
 
-        <b-card title="Want to integrate JWT?">
-            <b-card-text
-            >We carefully crafted JWT flow so you can implement JWT with ease and
-                with minimum efforts.</b-card-text>
-            <b-card-text
-            >Please read our JWT Documentation to get more out of JWT
-                authentication.</b-card-text>
-        </b-card>
-        <b-card>
-            <clock-with-task class=""></clock-with-task>
-        </b-card>
+                    <div class="text-left d-flex justify-content-between w-100 mt-1" >
+                        <div>
+                            Nov opis bla bla
+                            <div style="font-size: 0.7rem;">asdasdas</div>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-end ml-1">
+                            <span class="badge badge-pill" style="background: #6ec193 !important; margin-bottom: 5px;">20.8.2020</span>
+                        </div>
+                    </div>
+
+                    <div class="text-left d-flex justify-content-between w-100 mt-1" >
+                        <div>
+                            Jutrisni task adsa
+                            <div style="font-size: 0.7rem;">fddsfysfgada</div>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-end ml-1">
+                            <span class="badge badge-pill" style="background: #feb449 !important; margin-bottom: 5px;">21.8.2020</span>
+                        </div>
+                    </div>
+
+                    
+                    <div class="text-left d-flex justify-content-between w-100 mt-1" >
+                        <div>
+                            Task enkrat bla bla
+                            <div style="font-size: 0.7rem;">fddsfysfgada dasas</div>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-end ml-1">
+                            <span class="badge badge-pill" style="background: #53afbe !important; margin-bottom: 5px;">26.8.2020</span>
+                        </div>
+                    </div>
+
+                    <div class="text-left d-flex justify-content-between w-100 mt-1" >
+                        <div>
+                            FRi izpit juhejs juhjes
+                            <div style="font-size: 0.7rem;">fddsfysfgada dasas 123123</div>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-end ml-1">
+                            <span class="badge badge-pill" style="background: #53afbe !important; margin-bottom: 5px;">29.8.2020</span>
+                        </div>
+                    </div>
+
+                    <div class="text-left d-flex justify-content-between w-100 mt-1" >
+                        <div>
+                            Naloga tpo juhej
+                            <div style="font-size: 0.7rem;">fddsfysfgada</div>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-end ml-1">
+                            <span class="badge badge-pill" style="background: #feb449 !important; margin-bottom: 5px;">30.9.2020</span>
+                        </div>
+                    </div>
+
+                    <div class="text-left d-flex justify-content-between w-100 mt-1" >
+                        <div>
+                            BOZICEK
+                            <div style="font-size: 0.7rem;">JUHEJ</div>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-end ml-1">
+                            <span class="badge badge-pill" style="background: #fe5c36 !important; margin-bottom: 5px;">24.12.2020</span>
+                        </div>
+                    </div>
+
+                    <div class="text-left d-flex justify-content-between w-100 mt-1" >
+                        <div>
+                            NOVO LETO JUHEJ
+                            <div style="font-size: 0.7rem;">JUHEJ</div>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-end ml-1">
+                            <span class="badge badge-pill" style="background: #feb449 !important; margin-bottom: 5px;">31.12.2020</span>
+                        </div>
+                    </div>
+
+                    <div class="text-left d-flex justify-content-between w-100 mt-1" >
+                        <div>
+                            Konc semestra
+                            <div style="font-size: 0.7rem;">fddsfysfgada dasas 123123</div>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-end ml-1">
+                            <span class="badge badge-pill" style="background: #53afbe !important; margin-bottom: 5px;">1.1.2021</span>
+                        </div>
+                    </div>
+
+                    <div class="text-left d-flex justify-content-between w-100 mt-1" >
+                        <div>
+                            konc faksa
+                            <div style="font-size: 0.7rem;">fddsfysfgada dasas 123123</div>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-end ml-1">
+                            <span class="badge badge-pill" style="background: #6ec193 !important; margin-bottom: 5px;">2.4.2021</span>
+                        </div>
+                    </div>
+
+                    <div class="text-left d-flex justify-content-between w-100 mt-1" >
+                        <div>
+                            Sluzba
+                            <div style="font-size: 0.7rem;">fddsfysfgada dasas 123123</div>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-end ml-1">
+                            <span class="badge badge-pill" style="background: #fe5c36 !important; margin-bottom: 5px;">31.8.2021</span>
+                        </div>
+                    </div>
+                </b-card>
+
+            </div>
+            <div class="col-lg-4 col-4 h-100">
+                <calendar trim-weeks class="w-100" :class="isDark? 'dark-theme': ''"> </calendar>
+                <b-card class="w-100 mt-2">
+                    <div class="row py-1 item" >
+                        <div class="col-2 d-flex justify-content-center align-items-center">
+                            <b-form-checkbox
+                            ></b-form-checkbox>
+                        </div>
+                        <div class="col-10 text-left d-flex justify-content-between" style="cursor: pointer;">
+                            <div>
+                                Test
+                                <div style="font-size: 0.7rem;">test description</div>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-end ml-1">
+                                <div  class="text-right" >
+                                    <span class="badge badge-pill mr-1" style="background: #53afbe !important; margin-bottom: 5px;">Important</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row py-1 item" >
+                        <div class="col-2 d-flex justify-content-center align-items-center">
+                            <b-form-checkbox
+                            ></b-form-checkbox>
+                        </div>
+                        <div class="col-10 text-left d-flex justify-content-between" style="cursor: pointer;">
+                            <div>
+                                Test
+                                <div style="font-size: 0.7rem;">test description</div>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-end ml-1">
+                                <div  class="text-right" >
+                                    <span class="badge badge-pill mr-1" style="background: #53afbe !important; margin-bottom: 5px;">Important</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row py-1 item" >
+                        <div class="col-2 d-flex justify-content-center align-items-center">
+                            <b-form-checkbox
+                            ></b-form-checkbox>
+                        </div>
+                        <div class="col-10 text-left d-flex justify-content-between" style="cursor: pointer;">
+                            <div>
+                                Test
+                                <div style="font-size: 0.7rem;">test description</div>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-end ml-1">
+                                <div  class="text-right" >
+                                    <span class="badge badge-pill mr-1" style="background: #53afbe !important; margin-bottom: 5px;">Important</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row py-1 item" >
+                        <div class="col-2 d-flex justify-content-center align-items-center">
+                            <b-form-checkbox
+                            ></b-form-checkbox>
+                        </div>
+                        <div class="col-10 text-left d-flex justify-content-between" style="cursor: pointer;">
+                            <div>
+                                Test
+                                <div style="font-size: 0.7rem;">test description</div>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-end ml-1">
+                                <div  class="text-right" >
+                                    <span class="badge badge-pill mr-1" style="background: #53afbe !important; margin-bottom: 5px;">Important</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row py-1 item" >
+                        <div class="col-2 d-flex justify-content-center align-items-center">
+                            <b-form-checkbox
+                            ></b-form-checkbox>
+                        </div>
+                        <div class="col-10 text-left d-flex justify-content-between" style="cursor: pointer;">
+                            <div>
+                                Test
+                                <div style="font-size: 0.7rem;">test description</div>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-end ml-1">
+                                <div  class="text-right" >
+                                    <span class="badge badge-pill mr-1" style="background: #53afbe !important; margin-bottom: 5px;">Important</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </b-card>
+            </div>
+            <div class="col-lg-5 col-7">
+                <b-card class="w-100 h-100">
+                    <div class="text-center" style="font-size: 1.5rem;">Pregled dnevnih opravil</div>
+                    <clock-with-task class=""></clock-with-task>
+                </b-card>
+            </div>
+        </div>
+       
     </div>
 </template>
 
 <script>
-    import { BCard, BCardText, BLink } from 'bootstrap-vue'
+    import { BCard, BFormCheckbox } from 'bootstrap-vue'
     import ClockWithTask from './Components/ClockWithTask.vue'
+    import { Calendar } from 'v-calendar'
+    import useAppConfig from '@core/app-config/useAppConfig'
+    import { computed } from '@vue/composition-api'
 
     export default {
         components: {
             BCard,
-            BCardText,
-            BLink,
-            ClockWithTask
+            ClockWithTask,
+            Calendar,
+            BFormCheckbox
+        },
+        setup() {
+            const { skin } = useAppConfig()
+
+            const isDark = computed(() => skin.value === 'dark')
+
+            return { skin, isDark }
         }
     }
 </script>
@@ -49,3 +249,36 @@
 <style lang="scss" scoped>
 
 </style>>
+
+<style lang="scss">
+    .mojCard .card-body{
+        padding-bottom: 0px !important;
+    }
+    .vc-container {
+        border-radius: 0;
+        width: 100%;
+        border: none;
+
+        .vc-day{
+            color: #53afbe !important;
+        }
+        .vc-weekday{
+            color: #feb449 !important;
+        }
+        .vc-year{
+            color: #6ec193 !important;
+        }
+        .vc-title{
+            color: #6ec193 !important;
+        }
+    }
+
+    .dark-theme.vc-container {
+        background: #2e3134 !important;
+
+        .vc-arrow:hover{
+            background: #282A2D;
+        }
+       
+    }
+</style>
