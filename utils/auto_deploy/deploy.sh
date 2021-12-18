affc49f722a5158eed27762d808b79bea4c1b104
nikp00 <nik.princic@gmail.com>
2021-12-18 23:20:11 +0100

    Fixed deploy scripts

--- utils/auto_deploy/deploy.sh
+++ utils/auto_deploy/deploy.sh
@@ -5,3 +5,7 @@ docker login -u $(printenv DOCKERHUB_USERNAME) -p $(printenv DOCKERHUB_PASSWORD)
 docker-compose down &&
 docker-compose pull &&
 docker-compose up -d;
+
+# webhook -hooks /root/TPO/auto_deploy/hooks.json -verbose
+# tmux new -s webhook_session
+# tmux attach -t webhook_session
